b2deda5b3a5e63e0b5a0f6682cbb0f4814f6071e
van jordan <vanjordan46@hotmail.com>
2019-05-13 13:10:00 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home}>Home</Route>
+    <Route path="/about" component={About}>About</Route>
+    <Route path="/contact" component={Contact}>Contact</Route>
+    
   </div>
 );
 
